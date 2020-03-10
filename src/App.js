afbb0583ee2340603c29afa3b1847f644d797e2a
Eric Kim <hyeokjungkim@gmail.com>
2020-03-10 14:02:55 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import './App.css';
+
+import Header from './components/Header'
+import ToyForm from './components/ToyForm'
+import ToyContainer from './components/ToyContainer'
+
+import data from './data'
+
+
+class App extends React.Component{
+
+  state = {
+    display: false
+  }
+
+  handleClick = () => {
+    let newBoolean = !this.state.display
+    this.setState({
+      display: newBoolean
+    })
+  }
+
+  render(){
+    return (
+      <>
+        <Header/>
+        { this.state.display
+            ?
+          <ToyForm/>
+            :
+          null
+        }
+        <div className="buttonContainer">
+          <button onClick={this.handleClick}> Add a Toy </button>
+        </div>
+        <ToyContainer/>
+      </>
+    );
+  }
+
+}
+
+export default App;
---
+++ src/components/ToyCard.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class ToyCard extends Component {
+
+  render() {
+    return (
+      <div className="card">
+        <h2>{'' /* Toy's Name */}</h2>
+        <img src={'' /* Toy's Image */} alt={/* Toy's Name */} className="toy-avatar" />
+        <p>{'' /* Toy's Likes */} Likes </p>
+        <button className="like-btn">Like {'<3'}</button>
+        <button className="del-btn">Donate to GoodWill</button>
+      </div>
+    );
+  }
+
+}
+
+export default ToyCard;
