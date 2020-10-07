86d8e3833bd6847ef7227aff4934a5a18028d37c
HyeokJungKim <31145202+HyeokJungKim@users.noreply.github.com>
2020-10-07 15:22:06 -0400

    Update ToyCard.jsx

--- src/components/ToyCard.jsx
+++ src/components/ToyCard.jsx
@@ -6,7 +6,7 @@ class ToyCard extends Component {
     return (
       <div className="card">
         <h2>{'' /* Toy's Name */}</h2>
-        <img src={'' /* Toy's Image */} alt={/* Toy's Name */} className="toy-avatar" />
+        <img src={'' /* Toy's Image */} alt={'' /* Toy's Name */} className="toy-avatar" />
         <p>{'' /* Toy's Likes */} Likes </p>
         <button className="like-btn">Like {'<3'}</button>
         <button className="del-btn">Donate to GoodWill</button>
