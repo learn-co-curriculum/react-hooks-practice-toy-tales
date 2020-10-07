2a1a966772aef549915feb5906c9fa7ad8273359
HyeokJungKim <31145202+HyeokJungKim@users.noreply.github.com>
2020-10-07 14:54:29 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,8 +5,6 @@ import Header from './components/Header'
 import ToyForm from './components/ToyForm'
 import ToyContainer from './components/ToyContainer'
 
-import data from './data'
-
 
 class App extends React.Component{
 
