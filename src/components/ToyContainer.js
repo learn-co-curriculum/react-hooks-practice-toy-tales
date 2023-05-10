7c5502172306c3ddf9b72de8c15f83ff33ce6ce7
kush Muhamad <kushmohammed98@yahoo.com>
2023-05-10 21:30:13 +0300

    can get&post

--- src/components/ToyContainer.js
+++ src/components/ToyContainer.js
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ToyCard from "./ToyCard";
 
 function ToyContainer() {
+  const [toys, setToys] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:3001/toys")
+      .then((res) => res.json())
+      .then((data) => setToys(data));
+  }, []);
   return (
-    <div id="toy-collection">{/* Render the collection of ToyCards */}</div>
+    <div id="toy-collection">
+      { toys && toys.map((toy) => (
+        <div key={toy.id} className="card">
+          <h2>{toy.name}</h2>
+          <img src={toy.image} alt={toy.name} className="toy-avatar" />
+          <p>{toy.likes} Likes </p>
+          <button className="like-btn">Like {"<3"}</button>
+          <button className="del-btn">Donate to GoodWill</button>
+        </div>
+      ))}
+    </div>
   );
 }
 
